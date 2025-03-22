26a9bcf2be96e1b43b6144ca0d0471f3a84c81d3
Daniel Sundqvist <1891sunkan@gmail.com>
2025-03-22 12:00:21 +0000

    quiz/url submit all

--- quiz/urls.py
+++ quiz/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.quiz_list, name='quiz_list'),
     path('<int:quiz_id>/submit/', views.quiz_submit, name='quiz_submit'),
+    path('submit-all/', views.quiz_submit_all, name='quiz_submit_all'),
 ]
