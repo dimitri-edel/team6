ac5a32641c8a675e8034e7979c7e691b3c743b39
zalak786 <zalakjoshi786@gmail.com>
2025-03-24 02:00:32 +0000

    changes are done for main page

--- rippley/views.py
+++ rippley/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
-
+from about.models import About  # Import the About model
 def home(request):
-    return render(request, 'home.html')
+    women_profiles = About.objects.all()
+    return render(request, 'home.html', {'women_profiles': women_profiles})
 
 def team_view(request):
     return render(request, 'team.html')
+
+
+def about_list(request):
+    about_profiles = About.objects.all()
+    return render(request, 'about_list.html', {'about_profiles': about_profiles})
